3ad86e809271712cb4aac342c1f51910217e3a78
Serin Delaunay <serin.delaunay@gmail.com>
2017-03-06 06:06:05 +0000

    Alter main loop
    Don't wait for input before refreshing; advance state after each
    keypress.

--- fleohsis_ipython.py
+++ fleohsis_ipython.py
@@ -14,27 +14,36 @@ get_ipython().magic('aimport FPSLimiter')
 get_ipython().magic('autoreload')
 
 def main() -> None:
-    blt.open()
-    log = []
+    # init health
     ht = HealthTableau.HealthTableau()
     ht.insert_point(HealthPoints.Heart.copy())
     ht.insert_point(HealthPoints.Splanch.copy())
     ht.insert_point(HealthPoints.Phylactery.copy())
     ht.insert_point(HealthPoints.Arm.copy())
     ht.insert_point(HealthPoints.Arm.copy())
-    while not ht.is_dead():
-        log.append("{0}, {1}".format(repr(ht), "Dead" if ht.is_dead() else "Not dead"))
-        ht.inflict_damage()
+    # init log
+    log = []
     log.append("{0}, {1}".format(repr(ht), "Dead" if ht.is_dead() else "Not dead"))
-    for x in log:
-        print(x)
+    # draw first frame
+    blt.open()
     blt.printf(0,0,'[bbox=80x20]'+'\n'.join(log))
     fps_limiter = FPSLimiter.FPSLimiter()
     blt.set('window:title="FLEOHSIS ({0} FPS)"'.format(fps_limiter.get_fps()))
     blt.refresh()
-    while blt.read() != blt.TK_CLOSE:
+    # read-advance-display loop
+    stop = False
+    while not stop:
         fps_limiter.wait()
         blt.set('window:title="FLEOHSIS ({0} FPS)"'.format(fps_limiter.get_fps()))
+        while blt.has_input():
+            if blt.read() == blt.TK_CLOSE:
+                stop = True
+            else:
+                if not ht.is_dead():
+                    ht.inflict_damage()
+                    log.append("{0}, {1}".format(repr(ht), "Dead" if ht.is_dead() else "Not dead"))
+                blt.printf(0,0,'[bbox=80x20]'+'\n'.join(log))
+                blt.refresh()
     blt.close()
 
 if __name__ == '__main__':
