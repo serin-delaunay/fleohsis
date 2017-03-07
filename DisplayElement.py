88ed21a026940b53335823dacdfbc3303073bd21
Serin Delaunay <serin.delaunay@gmail.com>
2017-03-07 12:36:40 +0000

    Alter DisplayElement classes
    * Pass layer to draw method
    * Note namedtuple default hacks
    * Correct alignment for PrintArgs in BLT 0.15.1
    * Add Clickable class to identify DisplayElement targetted by mouse
    * Add DisplayGroup and derived classes to allow hierarchical display of
    Clickables

--- DisplayElement.py
+++ DisplayElement.py
@@ -1,14 +1,16 @@
 
 # coding: utf-8
-from typing import NamedTuple, Optional, Union
+from typing import NamedTuple, Optional, Union, Iterator, Dict, Callable, Any, TypeVar
 from bearlibterminal import terminal as blt
 from abc import ABCMeta, abstractmethod
 from vec import ivec
 from Colour import Colour, ColourRGBA
+from enum import Enum, auto
+from Rectangle import Rectangle
 
 class DisplayElement(metaclass=ABCMeta):
     @abstractmethod
-    def draw(self, xy : ivec) -> None: pass
+    def draw(self, xy : ivec, layer : int = 0) -> None: pass
 
 class PutArgs(DisplayElement,
               NamedTuple('PutArgs',
@@ -17,13 +19,19 @@ class PutArgs(DisplayElement,
                           ('fg_colour', Colour),
                           ('bg_colour', Optional[Colour])
                          ])):
-    def draw(self, xy : ivec) -> None:
+    def draw(self, xy : ivec, layer : int = 0) -> None:
+        blt.layer(layer)
         blt.color(self.fg_colour.blt_colour())
         if self.bg_colour is not None:
             blt.bkcolor(self.bg_colour.blt_colour())
         xy = xy + self.xy
         blt.put(xy.x, xy.y, self.char)
-PutArgs.__new__.__defaults__ = (ColourRGBA(255,255,255,255),None)
+PutArgs.__new__.__defaults__ = (ColourRGBA(255,255,255,255),None) # mypy hates this
+# When Python 3.6.1 arrives:
+#   * Remove __defaults__ assignment
+#   * Use class _PutArgs(NamedTuple)
+#   * Assign defaults in instance variable type hints
+#   * Use class PutArgs(NamedTuple, DisplayElement), to avoid metaclass conflict
 
 class PutExtArgs(DisplayElement,
                  NamedTuple('PutExtArgs',
@@ -33,23 +41,50 @@ class PutExtArgs(DisplayElement,
                              ('fg_colour', Colour),
                              ('bg_colour', Optional[Colour])
                             ])):
-    def draw(self, xy : ivec) -> None:
+    def draw(self, xy : ivec, layer : int = 0) -> None:
         blt.color(self.fg_colour.blt_colour())
         if self.bg_colour is not None:
             blt.bkcolor(self.bg_colour.blt_colour())
         xy = xy + self.xy
         blt.put_ext(xy.x, xy.y, self.dxy.x, self.dxy.y, self.char)
-PutExtArgs.__new__.__defaults__ = (ColourRGBA(255,255,255,255),None)
+PutExtArgs.__new__.__defaults__ = (ColourRGBA(255,255,255,255),None) # mypy hates this
+# When Python 3.6.1 arrives:
+#   * Remove __defaults__ assignment
+#   * Use class _PuExttArgs(NamedTuple)
+#   * Assign defaults in instance variable type hints
+#   * Use class PutExtArgs(NamedTuple, DisplayElement), to avoid metaclass conflict
+
+class TextAlignmentH(Enum):
+    Left = blt.TK_ALIGN_LEFT
+    Centre = blt.TK_ALIGN_CENTER
+    Right = blt.TK_ALIGN_RIGHT
+    Default = blt.TK_ALIGN_DEFAULT
+class TextAlignmentV(Enum):
+    Top = blt.TK_ALIGN_TOP
+    Middle = blt.TK_ALIGN_MIDDLE
+    Bottom = blt.TK_ALIGN_BOTTOM
+    Default = blt.TK_ALIGN_DEFAULT
+
+class TextAlignment(NamedTuple('TextAlignment', [('horizontal', TextAlignmentH), ('vertical', TextAlignmentV)])):
+    def code(self) -> int:
+        return self.horizontal.value + self.vertical.value
+TextAlignment.__new__.__defaults__ = (TextAlignmentH.Default, TextAlignmentV.Default) # mypy hates this
+# When Python 3.6.1 arrives:
+#   * Remove __defaults__ assignment
+#   * Use class TextAlignment(NamedTuple)
+#   * Assign defaults in instance variable type hints
 
 class PrintArgs(DisplayElement,
                 NamedTuple('PrintArgs',
                            [('text', str),
                             ('xy', ivec),
                             ('bbox', Optional[ivec]),
-                             ('fg_colour', Colour),
-                             ('bg_colour', Optional[Colour])
+                            ('align_h', TextAlignmentH),
+                            ('align_v', TextAlignmentV),
+                            ('fg_colour', Colour),
+                            ('bg_colour', Optional[Colour])
                            ])):
-    def draw(self, xy : ivec) -> None:
+    def draw(self, xy : ivec, layer : int = 0) -> None:
         blt.color(self.fg_colour.blt_colour())
         if self.bg_colour is not None:
             blt.bkcolor(self.bg_colour.blt_colour())
@@ -57,5 +92,60 @@ class PrintArgs(DisplayElement,
         if self.bbox is None:
             blt.print_(xy.x, xy.y, self.text)
         else:
-            blt.print_(xy.x, xy.y, '[bbox={0}x{1}]'.format(self.bbox.x, self.bbox.y) + self.text)
-PrintArgs.__new__.__defaults__ = (None, ColourRGBA(255,255,255,255),None)
+            blt.print_(xy.x, xy.y, self.text, self.bbox.x, self.bbox.y, self.align_h.value + self.align_v.value)
+PrintArgs.__new__.__defaults__ = (None, TextAlignmentH.Default, TextAlignmentV.Default, 
+                                  ColourRGBA(255,255,255,255),None) # mypy hates this
+# When Python 3.6.1 arrives:
+#   * Remove __defaults__ assignment
+#   * Use class _PrintArgs(NamedTuple)
+#   * Assign defaults in instance variable type hints
+#   * Use class PrintArgs(NamedTuple, DisplayElement), to avoid metaclass conflict
+
+T = TypeVar('T',bound='Clickable')
+class Clickable(object):
+    def __init__(self, element : DisplayElement,
+                 mouse_rect : Rectangle = Rectangle(ivec(0,0), ivec(0,0)),
+                 signals : Dict[str, Callable[[ivec,int,Dict[str,Any]],Any]] = {}) -> None:
+        self.element = element
+        self.mouse_rect = mouse_rect
+        self.signals = signals.copy()
+    def find_target(xy : ivec, signal : int) -> Optional[T]:
+        if xy in self.mouse_rect:
+            if isinstance(self.element, DisplayGroup):
+                for clickable in self.element:
+                    result = clickable.find_target(xy - self.mouse_rect.top_left)
+                    if result is not None:
+                        return result
+            if signal in self.signals:
+                return self
+        return None
+
+class DisplayGroup(DisplayElement, metaclass=ABCMeta):
+    def __init__(self, xy : ivec) -> None:
+        self.clear_elements()
+        self.layer = 0
+        self.xy = xy
+    def draw(self, xy : ivec, layer : int = 0) -> None:
+        xy = self.xy + xy
+        for clickable in self:
+            element = clickable.element
+            if isinstance(element, DisplayGroup):
+                element.draw(xy, layer + 1)
+            else:
+                element.draw(xy, layer)
+    @abstractmethod
+    def clear_elements(self) -> None: pass
+    @abstractmethod
+    def __iter__(self) -> Iterator[Clickable]: pass
+
+class DisplayList(DisplayGroup):
+    def clear_elements(self) -> None:
+        self.elements = []
+    def __iter__(self) -> Iterator[Clickable]:
+        return iter(self.elements)
+
+class DisplayDict(DisplayGroup):
+    def clear_elements(self) -> None:
+        self.elements = {}
+    def __iter__(self) -> Iterator[Clickable]:
+        return iter(self.elements.values())
